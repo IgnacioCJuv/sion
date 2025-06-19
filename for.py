c57fb6dac77b1b043043602c0cffd2b215422382
IgnacioCJuv <ignacio.vidalr@alumnos.uv.cl>
2025-06-19 11:20:57 -0400

    Update and rename for.py to for/for.py

--- for.py
+++
@@ -1,2 +0,0 @@
-for indice in range (9,-1,-1): #Permite un grupo de valores, en este caso numeros
-        print(indice) #Imprime una losta iniciendo desde el primer valor ingresado, hasta el segundo ingresado
---
+++ for/for.py
@@ -0,0 +1,2 @@
+for indice in range (9,-1,-1): #Permite un grupo de valores, en este caso numeros
+        print(indice) #Imprime una losta iniciendo desde el primer valor ingresado, hasta el segundo ingresado
